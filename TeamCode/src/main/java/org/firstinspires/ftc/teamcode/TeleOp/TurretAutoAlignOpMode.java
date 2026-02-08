2208926edaa4e9f449b091bda82322122650b9ce
Andrea <rasquinha.andrea@gmail.com>
2026-02-07 19:19:48 -0600

    Turret Auto Align
    added turret logic
    
    Not finished. Have question.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/TurretAutoAlignOpMode.java
@@ -0,0 +1,22 @@
+package org.firstinspires.ftc.teamcode.TeleOp;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+public class TurretAutoAlignOpMode extends OpMode {
+    private 
+
+
+    @Override
+    public void init() {
+
+    }
+
+    public void start() {
+
+    }
+
+    @Override
+    public void loop() {
+
+    }
+}
